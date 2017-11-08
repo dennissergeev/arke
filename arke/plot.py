0e5934931f6b326a14890fa6248839e93edfb139
Dennis Sergeev <dennis.sergeev@gmail.com>
2017-11-08 22:04:37 +0000

    add docstrings to plot.py

--- arke/plot.py
+++ arke/plot.py
@@ -15,7 +15,7 @@ from .cart import lcc_map_grid
 
 def make_axesgrid(vrbls_lists, axsize=8, axes_pad=0.2):
     """
-    TODO: docstring
+    TODO: deprecate?
     """
     nplots = len(vrbls_lists)
     nrows = int(np.sqrt(nplots))
@@ -39,7 +39,31 @@ def make_axesgrid(vrbls_lists, axsize=8, axes_pad=0.2):
 
 def prepare_map(vrbls_lists, geoax=False, axsize=8):
     """
-    TODO: docstring
+    Prepare grid of subplots for a list of lists of cubes
+
+    Parameters
+    ----------
+    vrbls_lists: list of lists of cubes
+        List of lists or cubelists each of which is plotted in one subplot.
+        For example, it can be a list of model experiments output:
+        [
+          [<iris 'Cube' of air_pressure / (Pa) (model_level_number: 40; grid_latitude: 600; grid_longitude: 600)>,
+           <iris 'Cube' of air_potential_temperature / (K) (model_level_number: 40; grid_latitude: 600; grid_longitude: 600)>,
+           <iris 'Cube' of geopotential_height / (m) (model_level_number: 40; grid_latitude: 600; grid_longitude: 600)>],
+          [<iris 'Cube' of air_pressure / (Pa) (model_level_number: 40; grid_latitude: 600; grid_longitude: 600)>,
+           <iris 'Cube' of air_potential_temperature / (K) (model_level_number: 40; grid_latitude: 600; grid_longitude: 600)>,
+           <iris 'Cube' of geopotential_height / (m) (model_level_number: 40; grid_latitude: 600; grid_longitude: 600)>]
+        ],
+        For each of the 2 lists here a subplot will be created and 3 variables will be plotted. 
+    geoax: bool, optional (default False)
+        Make subplots `cartopy.mpl.geoaxes.GeoAxes` (see `lcc_map_grid()`)
+    axsize: float, optional (default 8)
+        Subplot size in inches
+    Returns
+    -------
+    Figure, AxesGrid, dict
+        Parent figure and grid of subplots
+        and a dictionary with transform keyword if geoax is True
     """
     nplots = len(vrbls_lists)
     nrows = int(np.sqrt(nplots))
@@ -103,6 +127,24 @@ def prepare_map(vrbls_lists, geoax=False, axsize=8):
 
 
 def add_xaxis_below(parent_ax, xtick_array, xlab_array, shift_down):
+    """
+    Add x-axis parallel to the parent x-axis
+
+    Parameters
+    ----------
+    parent_ax: matplotlib.axes._subplots.AxesSubplot
+        Parent axes
+    xtick_array: numpy.ndarray or list
+        Sequence of x-tick positions
+    xlab_array: numpy.ndarray or list
+        Sequence of labels for x-ticks
+    shift_down: int
+        Number of points to shift the axis down
+    Returns
+    -------
+    matplotlib.axes._subplots.AxesSubplot
+        Added x-axis
+    """
     newax = parent_ax.twiny()
     newax.set_xticks(xtick_array)
     newax.set_xticklabels(xlab_array)
